9acd8feeab3c0668f43d5e899ff68824116182ee
Kai Windischman <kwindischman@gmail.com>
2019-06-20 21:11:56 -0400

    Dynamically calls commands and handles incorrect usage of the commands

--- index.js
+++ index.js
@@ -1,15 +1,79 @@
+const fs = require('fs');
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
-const client = new Discord.Client();
+const {prefix, token} = require('./config.json');
 
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./src/commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+const cooldowns = new Discord.Collection();
+//logs that the bot has logged in
+client.login(token);
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
-})
+});
 
+/**
+ * Every time a message is sent in the server, the bot reads it with this method.
+ * If the message begins with our prefix, we wee if we can run a command with it.
+ * If not, then the bot will tell the user that the specified command is nto valid.
+ *
+ * @param message: This is a string of the user's message
+ * @return : null
+ */
 client.on('message', message => {
-    if (message.content === 'ping') {
-        message.reply('pong');
+    if (!message.content.startswith("!!")) {
+        return
     }
-})
 
-client.login(token);
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    if (!client.commands.has(commandName)) return;
+
+    const command = client.commands.get(commandName);
+
+    if (command.args && !args.length) {
+        let reply = `You didn't provide any arguments, ${message.author}!`;
+
+        if (command.usage) {
+            reply += `\nThe proper usage would be: ${prefix}${command.name} ${command.usage}`;
+        }
+        return message.channel.send(reply);
+    }
+
+    if (command.guildOnly && message.channel.type !== 'text') {
+        return message.reply('I can\'t execute that command inside DMs!');
+    }
+
+    if (!cooldowns.has(command.name)) {
+        cooldowns.set(command.name, new Discord.Collection());
+    }
+
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.name);
+    const cooldownAmount = (command.cooldown || 3) * 1000;
+
+    if (timestamps.has(message.author.id)) {
+        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+        if (now < expirationTime) {
+            const timeLeft = (expirationTime - now) / 1000;
+            return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+        }
+    }
+    timestamps.set(message.author.id, now);
+    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+    try {
+        command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('there was an error trying to execute that command!');
+    }
+});
---
+++ src/commands/addevent.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'addevent',
+    description: 'Adds an event for the bot to keep track of!',
+    args: true,
+    usage: '{eventname} {date}',
+    guildOnly: true,
+    execute(message, args) {
+//have the bot display the people that are attending the event by checking through the emoji reactions
+    }
+};
